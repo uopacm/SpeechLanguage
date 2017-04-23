403b6cf0709a58bff54cec5dec8f3b01b32d19da
Jamie Lynn Culilap <j_culilap1@u.pacific.edu>
2017-04-23 15:35:55 -0700

    Can now choose random phrase from phrases.txt, space bar action implemented,and UI changes

--- applicationScreen.py
+++ applicationScreen.py
@@ -1,16 +1,29 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QLabel,QSizePolicy, QGridLayout
+from PyQt5 import QtGui
+
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QLabel, QGridLayout
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import *
+import random
 
 # Skeleton source from online
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.isRecording = False
+        self.left = 500
+        self.top = 300
+        self.windowWidth = 700
+        self.windowHeight = 500
 
         self.initUI()
 
+        # Have run() handle all methods
+        self.run()
+
     def initUI(self):
+        self.setGeometry(self.left, self.top, self.windowWidth, self.windowHeight)
+        self.setWindowTitle('Main Menu')
 
         exitAction = QAction(QIcon('exit.png'), '&Exit', self)
         exitAction.setShortcut('Ctrl+Q')
@@ -23,19 +36,65 @@ class App(QMainWindow):
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAction)
 
-        self.setGeometry(500, 300, 500, 500)
-        self.setWindowTitle('Main Menu')
-
-        self.label = QLabel("hello!", self)
-        self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.label.setStyleSheet("QLabel {background-color: black; color : white}")
-
+        # Not quite sure about the differences between all the layout options....
         self.layout = QGridLayout()
-        self.layout.addWidget(self.label)
-        self.label.move(150, 150)
+
+        # Widgets used for application
+        self.spacePressed = QLabel("Record on.", self)
+        self.spaceNotPressed = QLabel("Record off.", self)
+        self.phrase = QLabel(self)
+        self.font = QtGui.QFont()
+
+        # Add widgets to layout
+        self.layout.addWidget(self.phrase)
+        self.layout.addWidget(self.spacePressed)
+        self.layout.addWidget(self.spaceNotPressed)
+
+        # Hide all widgets for now
+        self.phrase.hide()
+        self.spacePressed.hide()
+        self.spaceNotPressed.hide()
 
         self.show()
+
+    def run(self):
+        self.showPhrases()
+
+    def showPhrases(self):
+        # Read in first paragraph of phrases.txt
+        text = open('phrases.txt').read().splitlines()
+        line = random.choice(text) # Choose random line
+
+        # Edit phrase
+        self.phrase.setText(line)
+        self.font.setPointSize(12)
+        self.phrase.setWordWrap(True)
+        self.phrase.setFont(self.font)
+        self.phrase.setAlignment(Qt.AlignCenter)
+        self.phrase.adjustSize()
+        self.phrase.move(self.width()/4, self.height()/4)
+
+        self.phrase.show()
+
+    def keyPressEvent(self, event):
+
+        # TODO: WILL CHANGE ACTIONS LATER!!!
+        # Temporary actions to show spacebar workes for now.
+
+        if event.key() == Qt.Key_Space and self.isRecording == False:
+            self.spaceNotPressed.hide()
+            self.spacePressed.move(self.width() / 4, self.height() - 200)
+            self.spacePressed.setFont(self.font)
+            self.spacePressed.adjustSize()
+            self.spacePressed.show()
+            self.isRecording = True
+        elif (event.key() == Qt.Key_Space and self.isRecording):
+            self.spacePressed.hide()
+            self.spaceNotPressed.move(self.width() / 2, self.height() - 200)
+            self.spaceNotPressed.setFont(self.font)
+            self.spaceNotPressed.adjustSize()
+            self.spaceNotPressed.show()
+            self.isRecording = False
 
 
 if __name__ == '__main__':
