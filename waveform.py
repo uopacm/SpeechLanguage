b231397ffa33c6b916832fe1d181e120aeecd219
Jeffrey Dwyer <Jeffrey Dwyer>
2017-04-25 09:49:06 -0700

    Reverted the waveform.py to output to file

--- waveform.py
+++ waveform.py
@@ -2,34 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import wave
 import sys
-import tempfile
 
+# Give filename without extension
 def generatePng(filename):
-	spf = wave.open(filename, 'r')
+        spf = wave.open(filename + '.wav', 'r')
 	
-	signal = spf.readframes(-1)
-	signal = np.fromstring(signal, 'Int16')
-	fs = spf.getframerate()
+        signal = spf.readframes(-1)
+        signal = np.fromstring(signal, 'Int16')
+        fs = spf.getframerate()
 	
-	if spf.getnchannels() == 2:
-		print('Just mono files')
-		# sys.exit(0) # Running this in pyhcharm
+        if spf.getnchannels() == 2:
+            print('Just mono files')
+	    # sys.exit(0) # Running this in pyhcharm
 
 	# In seconds (Algorithm taken from online)
-	# Timing is extended.... test with more files
-	Time = np.linspace(0, len(signal)/fs, num=len(signal))
-	
-	plt.figure(1)
-	plt.title('Signal wave..')
-	plt.plot(Time,signal)
-	
-	#plt.savefig('test.png')
-	
-	temp = tempfile.NamedTemporaryFile()
-	try:
-		plt.savefig(temp.name)
-	
-	return temp
+	# Timing is extended.... test with more
+        Time = np.linspace(0, len(signal)/fs, num=len(signal))
+        plt.figure(1)
+        plt.title('Signal wave..')
+        plt.tick_params(axis='y', which='both', left='off',labelleft='off')
+        plt.plot(Time,signal)
+        plt.savefig(filename + '.png')
 
-'''if __name__ == '__main__':
-	generatePng('test.wav')'''
