b89c603f7f2d4925c9165ae3fe20b93b404aff0a
theMusicalGamer <pianojammer@comcast.net>
2017-05-01 16:17:47 -0700

    Fixed plot overlay issue

--- waveform.py
+++ waveform.py
@@ -6,11 +6,11 @@ import sys
 # Give filename without extension
 def generatePng(filename):
         spf = wave.open(filename + '.wav', 'r')
-	
+
         signal = spf.readframes(-1)
         signal = np.fromstring(signal, 'Int16')
         fs = spf.getframerate()
-	
+
         if spf.getnchannels() == 2:
             print('Just mono files')
 	    # sys.exit(0) # Running this in pyhcharm
@@ -18,9 +18,9 @@ def generatePng(filename):
 	# In seconds (Algorithm taken from online)
 	# Timing is extended.... test with more
         Time = np.linspace(0, len(signal)/fs, num=len(signal))
+        plt.gcf().clear()
         plt.figure(1)
         plt.title(filename)
         plt.tick_params(axis='y', which='both', left='off',labelleft='off')
         plt.plot(Time,signal)
         plt.savefig(filename + '.png')
-
