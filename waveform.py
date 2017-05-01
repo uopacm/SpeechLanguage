36e0d938ce82577842355bd804040115a15dd2f0
theMusicalGamer <pianojammer@comcast.net>
2017-05-01 16:46:00 -0700

    Fixed indents in waveform.py

--- waveform.py
+++ waveform.py
@@ -5,22 +5,22 @@ import sys
 
 # Give filename without extension
 def generatePng(filename):
-        spf = wave.open(filename + '.wav', 'r')
+    spf = wave.open(filename + '.wav', 'r')
 
-        signal = spf.readframes(-1)
-        signal = np.fromstring(signal, 'Int16')
-        fs = spf.getframerate()
+    signal = spf.readframes(-1)
+    signal = np.fromstring(signal, 'Int16')
+    fs = spf.getframerate()
 
-        if spf.getnchannels() == 2:
-            print('Just mono files')
-	    # sys.exit(0) # Running this in pyhcharm
+    if spf.getnchannels() == 2:
+        print('Just mono files')
+    # sys.exit(0) # Running this in pyhcharm
 
 	# In seconds (Algorithm taken from online)
 	# Timing is extended.... test with more
-        Time = np.linspace(0, len(signal)/fs, num=len(signal))
-        plt.gcf().clear()
-        plt.figure(1)
-        plt.title(filename)
-        plt.tick_params(axis='y', which='both', left='off',labelleft='off')
-        plt.plot(Time,signal)
-        plt.savefig(filename + '.png')
+    Time = np.linspace(0, len(signal)/fs, num=len(signal))
+    plt.gcf().clear()
+    plt.figure(1)
+    plt.title(filename)
+    plt.tick_params(axis='y', which='both', left='off',labelleft='off')
+    plt.plot(Time,signal)
+    plt.savefig(filename + '.png')
